d02c457452de2726e3c8d0a1ff932fc21f1323ed
kmuenpra <85464168+kmuenpra@users.noreply.github.com>
2021-06-24 21:34:03 -0400

    added Final project Xcode

--- Today/Today/ViewController.swift
+++ Today/Today/ViewController.swift
@@ -8,12 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
 
+    @IBOutlet weak var usernames: UILabel!
+    
+    @IBOutlet weak var profilePic: UIImageView!
+    
+    //Define "made up" username
+    
+    let name1 = "Orlando Hoillet"
+    let name2 = "Akio Fujita"
+    let name3 = "Ally Pickering"
+    let name4 = "Kasidit Muenprasitivej"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func findMatch(_ sender: Any) {
+        
+        let n = Int.random(in: 1...4)
+        
+        if (n == 1) {
+            usernames.text = name1;
+            profilePic.image = UIImage()
+            
+        } else if (n == 2) {
+            usernames.text = name2;
+            
+        } else if (n == 3) {
+            usernames.text = name3;
+            
+        } else if (n == 4) {
+            usernames.text = name4;
+        }
+        
+        
+    }
+    
 
 }
 
